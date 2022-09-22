44e4e3317403d4c4176822226402d7ace77aaaf9
kim <rlarkdgh1056@naver.com>
2022-09-22 18:06:59 +0900

    Initial commit

---
+++ src/main/java/sport/jok95bba/Jok95bbaApplication.java
@@ -0,0 +1,13 @@
+package sport.jok95bba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jok95bbaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jok95bbaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sport/jok95bba/Jok95bbaApplicationTests.java
@@ -0,0 +1,13 @@
+package sport.jok95bba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jok95bbaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
