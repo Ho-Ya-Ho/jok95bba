efc1bf99d3fa49a2f50d7efe7a3f085a62a13cce
kim <rlarkdgh1056@naver.com>
2022-09-23 13:41:04 +0900

    Commit

--- src/main/java/sport/jok95bba/basic/requestmapping/MappingController.java
+++ src/main/java/sport/jok95bba/basic/requestmapping/MappingController.java
@@ -18,17 +18,17 @@ public class MappingController {
         return "ok";
     }
 
-    @GetMapping("/log-test")
-    public String logTest() {
-        String name = "Spring";
-        logger.trace("trace log={}", name);
-        logger.debug("debug log={}", name);
-        logger.info(" info log={}", name);
-        logger.warn(" warn log={}", name);
-        logger.error("error log={}", name);
-//로그를 사용하지 않아도 a+b 계산 로직이 먼저 실행됨, 이런 방식으로 사용하면 X log.debug("String concat log=" + name);
-        return "ok";
-    }
+//    @GetMapping("/log-test")
+//    public String logTest() {
+//        String name = "Spring";
+//        logger.trace("trace log={}", name);
+//        logger.debug("debug log={}", name);
+//        logger.info(" info log={}", name);
+//        logger.warn(" warn log={}", name);
+//        logger.error("error log={}", name);
+////로그를 사용하지 않아도 a+b 계산 로직이 먼저 실행됨, 이런 방식으로 사용하면 X log.debug("String concat log=" + name);
+//        return "ok";
+//    }
 
     @GetMapping("/mapping/users/{userId}/orders/{orderId}")
     public String mappingPath(@PathVariable String userId, @PathVariable Long orderId) {
